019e7f1a2466ce5fda3302bfa958789b1c325d07
wswmsword <cjzjzzz@gmail.com>
2024-11-07 21:08:55 +0800

    improve dx of x/y transition prop

--- n-custom-x-motion-content.js
+++ n-custom-x-motion-content.js
@@ -2,12 +2,12 @@ import React from "react";
 import MotionContentWrapper from "./n-motion-content-wrapper";
 import CustomMotionItems from "./n-custom-motion-items";
 
-export default function CustomXMotionContent({ children, trans, ...contentWrapperProps }) {
+export default function CustomXMotionContent({ children, trans, xTrans, ...contentWrapperProps }) {
 
   return <MotionContentWrapper {...contentWrapperProps}>
     {({ transRunning }) =>
       <CustomMotionItems
-        trans={trans}
+        trans={xTrans || trans}
         transRunning={transRunning}>{children}</CustomMotionItems>}
   </MotionContentWrapper>;
 }
--- n-custom-y-motion-content.js
+++ n-custom-y-motion-content.js
@@ -2,9 +2,9 @@ import React from "react";
 import Items from "./n-items";
 import CustomMotionContentWrapper from "./n-custom-motion-content-wrapper";
 
-export default function CustomYMotionContent({ children, ...contentWrapperProps }) {
+export default function CustomYMotionContent({ children, yTrans, trans, ...contentWrapperProps }) {
 
-  return <CustomMotionContentWrapper {...contentWrapperProps}>
+  return <CustomMotionContentWrapper trans={yTrans || trans} {...contentWrapperProps}>
     {({ transitionBeforeStart }) =>
       <Items
         transitionBeforeStart={transitionBeforeStart}>{children}</Items>}
